bfda1baab65941f2648458521a7f08c5dca3ca5c
Jose Martin Rangel Espinoza <jose.rangelespinoza@mercadolibre.com.mx>
2021-04-21 13:53:45 -0500

    storage

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	// }
 
 	// myProduct := model.Product{}
-	// storage.DB().First(&myProduct, 3)
+	// storage.DB().First(&myProduct, 2)
 	// fmt.Println(myProduct)
 
 	// myProduct := model.Product{}
@@ -61,8 +61,22 @@ func main() {
 
 	// storage.DB().Delete(&myProduct)
 
-	myProduct := model.Product{}
-	myProduct.ID = 2
+	// myProduct := model.Product{}
+	// myProduct.ID = 2
 
-	storage.DB().Unscoped().Delete(&myProduct)
+	// storage.DB().Unscoped().Delete(&myProduct)
+
+	// storage.DB().Model(&model.InvoiceItem{}).AddForeignKey("product_id",
+	// 	"products(id)", "RESTRICT", "RESTRICT")
+	// storage.DB().Model(&model.InvoiceHeader{}).AddForeignKey("invoice_header_id",
+	// 	"invoice_headers(id)", "RESTRICT", "RESTRICT")
+
+	invoice := model.InvoiceHeader{
+		Client: "Martin Rangel",
+		InvoiceItems: []model.InvoiceItem{
+			{ProductID: 1},
+		},
+	}
+
+	storage.DB().Create(&invoice)
 }
